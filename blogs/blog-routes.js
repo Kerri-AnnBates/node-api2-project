b6bdaa007cec83605e93d23f0e984a734411efaa
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-03 16:24:55 -0500

    GET request for post by id

--- blogs/blog-routes.js
+++ blogs/blog-routes.js
@@ -14,4 +14,22 @@ router.get('/', (req, res) => {
         })
 })
 
+// GET post by a specific id ?
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db.findById(id)
+        .then(post => {
+            if(post.length > 0) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+        })
+        .catch(error => {
+            res.end();
+            res.status(500).json({error: "The post information could not be retrieved." });
+        })
+})
+
 module.exports = router;
