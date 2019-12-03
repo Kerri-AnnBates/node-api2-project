f7fa311ca6e7c62ceb242517d98e68e44bbc3ef9
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-03 16:03:46 -0500

    set up get request for all posts.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const blogRoutes = require('../blogs/blog-routes');
 const server = express();
+server.use(express.json());
 
 server.get('/', (req, res) => {
     res.send(`<h1>Welcome to my blog</h1>`);
 });
+
+server.use('/api/posts', blogRoutes);
 
 module.exports = server;
